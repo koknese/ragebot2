75f245d38e9b946534f5e404ec766ea7d6506987
Dainis <gatesby@protonmail.com>
2025-03-15 23:59:20 +0200

    complete

--- cogs/rageboard.py
+++ cogs/rageboard.py
@@ -21,18 +21,8 @@ class Postui(ui.Modal, title='Posting to Rageboard'):
    green = ui.TextInput(label='Make greentext', placeholder="Write 'true' to make the embed a green text.", style=discord.TextStyle.short, required=False)
 
    async def on_submit(self, interaction: discord.Interaction):
-       role = interaction.guild.get_role(booster_id)
-       def checkBooster():
-           if role in interaction.user.roles:
-               return True
-           else:
-               return False
-       
        await interaction.response.send_message(f'Sending to Rageboard...')
        default_color = discord.Colour(27010)
-       if checkBooster():
-           default_color = discord.Colour(14695336)
-       
        embed = discord.Embed(
            color=discord.Colour.dark_green() if self.green.value.lower() == "true" else default_color,
            description=self.body.value
@@ -45,7 +35,7 @@ class Postui(ui.Modal, title='Posting to Rageboard'):
            embed.set_thumbnail(url=self.image.value)
            
        unix_timestamp = (datetime.now() - datetime(1970, 1, 1)).total_seconds()
-       embed.set_footer(text=f"@Rageboard | {version}")
+       embed.set_footer(text=f"Rageboard | {version}")
        rageboard_channel = interaction.client.get_channel(rageboard_id)
        message = await rageboard_channel.send(f"{format_dt(datetime.now(), style='F')}", embed=embed)
        mid = message.id
@@ -58,6 +48,43 @@ class Postui(ui.Modal, title='Posting to Rageboard'):
        else:
            await interaction.response.send_message(f"{error}\n## Rageboard modal created successfully, but an ***extremely rare fatal error*** occured. Report this to the development team. Bailing out, you are on your own. Good luck.", ephemeral=True)
         
+class Postuipremium(ui.Modal, title='Posting to Rageboard with premium'):
+   body = ui.TextInput(label='Body text', placeholder="Rage about something here!", style=discord.TextStyle.long)
+   image = ui.TextInput(label='Image link', placeholder="Paste an image link here if you have one!", style=discord.TextStyle.short, required=False)
+   bigimage = ui.TextInput(label='Big image link', placeholder="Paste a bigger image link here if you have one!", style=discord.TextStyle.short, required=False)
+   green = ui.TextInput(label='Make greentext', placeholder="Write 'true' to make the embed a green text.", style=discord.TextStyle.short, required=False)
+
+   async def on_submit(self, interaction: discord.Interaction):
+       await interaction.response.send_message(f'Sending to Rageboard as a Premium user...')
+       default_color = discord.Colour(14695336)
+       
+       embed = discord.Embed(
+           color=discord.Colour.dark_green() if self.green.value.lower() == "true" else default_color,
+           description=self.body.value
+       )
+       embed.set_author(
+           name=f"{interaction.user}",
+           icon_url=interaction.user.avatar,
+       )
+       if self.image.value:
+           embed.set_thumbnail(url=self.image.value)
+           
+       if self.bigimage.value:
+           embed.set_image(url=self.bigimage.value)
+           
+       unix_timestamp = (datetime.now() - datetime(1970, 1, 1)).total_seconds()
+       embed.set_footer(text=f"Rageboard Premium | {version}", icon_url="https://cdn.discordapp.com/emojis/1350579789823742042.gif?size=80&quality=lossless")
+       rageboard_channel = interaction.client.get_channel(rageboard_id)
+       message = await rageboard_channel.send(f"{format_dt(datetime.now(), style='F')}", embed=embed)
+       mid = message.id
+       await message.add_reaction("⭐")
+       thread = await message.create_thread(name=f'Rageboard thread {mid}')
+
+   async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
+       if interaction.response.is_done():
+           print(f"Error occurred: {error}")
+       else:
+           await interaction.response.send_message(f"{error}\n## Rageboard modal created successfully, but an ***extremely rare fatal error*** occured. Report this to the development team. Bailing out, you are on your own. Good luck.", ephemeral=True)
 class Rageboard(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -69,8 +96,19 @@ class Rageboard(commands.Cog):
     )
     @app_commands.guilds(discord.Object(id=server_id))
     async def post(self, interaction:discord.Interaction):
-        modal = Postui()
-        await interaction.response.send_modal(modal)
+        role = interaction.guild.get_role(booster_id)
+        def checkBooster():
+            if role in interaction.user.roles:
+                return True
+            else:
+                return False
+            
+        if checkBooster():
+            modal = Postuipremium()
+            await interaction.response.send_modal(modal)
+        else:
+            modal = Postui()
+            await interaction.response.send_modal(modal)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Rageboard(bot))
