a25626ae8ef82e93cdcf9a91bbcd38b4fff47fd4
Dainis <gatesby@protonmail.com>
2025-03-21 22:37:50 +0200

    created group for tags

--- cogs/tags.py
+++ cogs/tags.py
@@ -4,49 +4,44 @@ import os.path
 import json
 from misc.paginator import Pagination
 from discord.ext import commands
-from discord import app_commands
-from dotenv import load_dotenv
 from discord import app_commands, Embed
+from dotenv import load_dotenv
+from discord.app_commands import Group, command
+from discord.ext.commands import GroupCog
 
 load_dotenv('../version', override=True)
 load_dotenv('../.config', override=True)
 server_id = os.getenv('CONFIG_SERVER_ID')
-tagbanned_id = os.getenv('CONFIG_TAGS_BANNED_ID')
 version = os.getenv('VERSION')
 
-class Tags(commands.Cog):
+class Tags(GroupCog, group_name='tags', group_description='tags, short snippets of information'):
     def __init__(self, bot):
         self.bot = bot
         self._last_member = None
         
-    @app_commands.command(name='create-tag', description='Create a tag!')
-    @app_commands.guilds(discord.Object(id=server_id))
+    @command(name='create', description='Create a tag!')
     async def createTag(self, interaction: discord.Interaction, name: str, content: str):
-        try:
-            if not os.path.exists("tags"):
-                os.makedirs("tags")
+        if not os.path.exists("tags"):
+            os.makedirs("tags")
 
-            path = f"tags/{name}.json"
-            fileExists = os.path.exists(path)
-            if not fileExists:
-                tag_dict = {
-                    "name": name,
-                    "creator": interaction.user.name,
-                    "content": content
-                }
+        path = f"tags/{name}.json"
+        fileExists = os.path.exists(path)
+        if not fileExists:
+            tag_dict = {
+                "name": name,
+                "creator": interaction.user.name,
+                "content": content
+            }
 
-                with open(f'tags/{name}.json', 'w') as f:
-                    json.dump(tag_dict, f, indent=4)
-                print(f"{interaction.user} created a tag")
-                await interaction.response.send_message(f"Tag {name} created!")
-            else:
-                await interaction.response.send_message(f"Tag {name} already exists!", ephemeral=True)
-        except discord.app_commands.CheckFailure:
-            await interaction.response.send_message(f"Something has went horribly wrong.", ephemeral=True)
+            with open(f'tags/{name}.json', 'w') as f:
+                json.dump(tag_dict, f, indent=4)
+            print(f"{interaction.user} created a tag")
+            await interaction.response.send_message(f"Tag {name} created!")
+        else:
+            await interaction.response.send_message(f"Tag {name} already exists!", ephemeral=True)
             
     
-    @app_commands.command(name='tag', description='Read a tag')
-    @app_commands.guilds(discord.Object(id=server_id))
+    @command(name='show', description='Read a tag')
     @app_commands.describe(tag="Tag to read")
     async def readTag(self, interaction: discord.Interaction, tag: str):
         try:
@@ -63,8 +58,7 @@ class Tags(commands.Cog):
             embed.set_footer(text=f"Ragecord Utils {version}")
             await interaction.response.send_message(embed=embed)
             
-    @app_commands.command(name='delete-tag', description='Delete a tag')
-    @app_commands.guilds(discord.Object(id=server_id))
+    @command(name='remove', description='Delete a tag')
     @discord.app_commands.checks.has_permissions(manage_messages=True)
     async def deleteTag(self, interaction: discord.Interaction, tag: str, reason: str):
         try:
@@ -80,8 +74,7 @@ class Tags(commands.Cog):
             await interaction.response.send_message(embed=embed)
     
     # What you're about to see is the prime definition of being held together by gum & string
-    @app_commands.command(name='list-tags', description='List tags')
-    @app_commands.guilds(discord.Object(id=server_id))
+    @command(name='list', description='List tags')
     async def show(self, interaction: discord.Interaction):
         def get_taglist():
             taglist = os.listdir("tags")
@@ -102,6 +95,6 @@ class Tags(commands.Cog):
 
         await Pagination(interaction, get_page).navegate()
 async def setup(bot: commands.Bot):
-    await bot.add_cog(Tags(bot))
+    await bot.add_cog(Tags(bot), guild=discord.Object(id=server_id))
         
         
